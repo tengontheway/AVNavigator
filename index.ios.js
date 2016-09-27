4fd0fae2f89c8db72aeacc1c6a7831c6b62c2485
TengZhengTao <teng_ontheway@163.com>
2016-09-27 23:05:48 +0800

    添加了简单的Navigator的页面跳转，能传递参数，也能通过callback回传参数

--- index.ios.js
+++ index.ios.js
@@ -2,31 +2,39 @@
  * Sample React Native App
  * https://github.com/facebook/react-native
  * @flow
+ *
  */
 
-import React from 'react';
+import React, {Component} from 'react';
 import {
+    AppRegistry,
     View,
     Navigator
 } from 'react-native';
+
+import utils from "./js/utils/Util"
+global.utils = utils
+
 import FirstPageComponent from './js/views/FirstPageComponent';
 
-export default class NavigatorDemo extends Component {
+class AVNavigator extends Component {
   render() {
     let defaultName = 'FirstPageComponent';
     let defaultComponent = FirstPageComponent;
     return (
         <Navigator
+            ref={nav => global.nav = nav}
             initialRoute={{ name: defaultName, component: defaultComponent }}
             configureScene={(route) => {
               return Navigator.SceneConfigs.VerticalDownSwipeJump;
             }}
             renderScene={(route, navigator) => {
-              let Component = route.component;
-              return <Component {...route.params} navigator={navigator} />
+              let Com = route.component;
+
+              return <Com  {...route.params} navigator={navigator} />
             }} />
     );
   }
 }
 
-AppRegistry.registerComponent('NavigatorDemo', () => NavigatorDemo);
+AppRegistry.registerComponent('AVNavigator', () => AVNavigator);
--- js/views/FirstPageComponent.js
+++ js/views/FirstPageComponent.js
@@ -2,24 +2,78 @@
  * Created by Qx on 9/27/16.
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-    AppRegistry,
     StyleSheet,
     Text,
-    View
+    View,
+    TouchableOpacity,
+    Navigator
 } from 'react-native';
 
+import SecondPageComponent from './SecondPageComponent'
 
 export default  class FirstPageComponent extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            id: 2,
+            user: null,
+        };
+    }
+
+    _pressButton() {
+        let _this = this
+
+        const {navigator} = this.props;
+        //为什么这里可以取得 props.navigator?请看上文:
+        //<Component {...route.params} navigator={navigator} />
+        //这里传递了navigator作为props
+        if (navigator) {
+            navigator.push({
+                name: 'SecondPageComponent',
+                component: SecondPageComponent,
+                //这里多出了一个 params 其实来自于<Navigator 里的一个方法的参数...
+                params: {
+                    id: this.state.id,
+                    //从SecondPageComponent获取user
+                    getUser: function (user) {
+                        _this.setState({
+                            user: user
+                        })
+                    }
+                },
+                animationType: Navigator.SceneConfigs.FloatFromRight
+            })
+        }
+    }
+
     render() {
-        return (
-            <View style={styles.container}>
-                <Text style={styles.welcome}>
-                    First page...
-                </Text>
-            </View>
-        );
+        if (this.state.user) {
+            return (
+                <View style={{marginTop: 50}}>
+                    <Text>用户信息: { JSON.stringify(this.state.user) }</Text>
+                </View>
+            );
+        } else {
+            return (
+                <View style={{
+                    flex: 1,
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    backgroundColor: 'blue'
+                }}>
+                    <TouchableOpacity onPress={this._pressButton.bind(this)}>
+                        <Text>点我跳转并传递id</Text>
+                    </TouchableOpacity>
+                    <Text style={styles.welcome}>
+                        FFirstPageComponent
+                    </Text>
+                </View>
+            )
+        }
     }
 }
 
